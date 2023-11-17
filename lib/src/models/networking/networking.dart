91deb73e2dd8650b5b9a10880ca818bcb62f7384
9init <emovent2e@gmail.com>
2023-11-17 16:53:56 +0200

    documented networking

--- lib/src/models/networking/networking.dart
+++ lib/src/models/networking/networking.dart
@@ -7,10 +7,12 @@ import 'package:path_provider/path_provider.dart';
 import 'package:vendor/src/models/networking/request_method.dart';
 import 'package:vendor/src/models/networking/results.dart';
 
+/// A singleton class for handling network requests using Dio.
 class Networking {
   static Networking? _self;
+
   // ignore: constant_identifier_names
-  static const String BASE_URL = "https://api.waspha.com/";
+  static const String BASE_URL = "https://api.waspha.com/vendor";
 
   static BaseOptions options = BaseOptions(
     baseUrl: BASE_URL,
@@ -21,16 +23,19 @@ class Networking {
   );
   final _dio = Dio(options);
 
-  static Networking instance() {
-    _self ??= Networking._();
-    return _self!;
-  }
-
+  /// Private constructor for singleton pattern.
   Networking._() {
     _self = this;
     prepareJar();
   }
 
+  /// Gets the singleton instance of the [Networking] class.
+  static Networking instance() {
+    _self ??= Networking._();
+    return _self!;
+  }
+
+  /// Initializes a cookie jar for managing cookies during network requests.
   Future<void> prepareJar() async {
     final Directory appDocDir = await getApplicationDocumentsDirectory();
     final String appDocPath = appDocDir.path;
@@ -41,6 +46,9 @@ class Networking {
     _dio.interceptors.add(CookieManager(jar));
   }
 
+  /// Makes a generic HTTP request with the specified [url], [requestMethod], and optional [data].
+  ///
+  /// Returns a [Result] containing either a [Response] on success or an [Exception] on failure.
   static Future<Result<Response, Exception>> call(
       url, RequestMethod requestMethod,
       {Object? data}) async {
@@ -56,16 +64,19 @@ class Networking {
     }
   }
 
+  /// Makes a POST request with the specified [url] and [data].
   static Future<Result<Response, Exception>> post(
       String url, dynamic data) async {
     return call(url, RequestMethod.POST, data: data);
   }
 
+  /// Makes a GET request with the specified [url] and optional [data].
   static Future<Result<Response, Exception>> get(
       String url, dynamic data) async {
     return call(url, RequestMethod.GET, data: data);
   }
 
+  /// Makes a DELETE request with the specified [url] and optional [data].
   static Future<Result<Response, Exception>> delete(
       String url, dynamic data) async {
     return call(url, RequestMethod.DELETE, data: data);
