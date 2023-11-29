bef532548d2e96211b2a4d445b371b4240721f63
MohamedMahmoud101198 <mohamed.mahmoud@appgain.io>
2023-11-29 02:54:40 +0200

    [feat]:added constance files color,dimensions as single responsibility classes

---
+++ lib/core/constans/dimension/dimensions.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AppDimensions {
+  // Text sizes
+  static double textSizeSmall = 14.sp;
+  static double textSizeMedium = 14.sp;
+  static double textSizeLarge = 14.sp;
+  static double textSizeExtraLarge = 14.sp;
+
+  // Heights
+  static double heightSmall = 20.h;
+  static double heightMedium = 40.h;
+  static double heightLarge = 60.h;
+  static double heightExtraLarge = 80.h;
+
+  // Widths
+  static double widthSmall = 50.w;
+  static double widthMedium = 100.w;
+  static double widthLarge = 150.w;
+  static double widthExtraLarge = 200.w;
+}
