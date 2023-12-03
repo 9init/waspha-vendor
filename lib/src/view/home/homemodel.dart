6d163cd14ae4ca2e46003765b381d4b6dca6762b
MohamedMahmoud101198 <mohamed.mahmoud@appgain.io>
2023-12-03 21:58:14 +0200

    [feat]:implement switch button for each widget (delivery-pickup-box) as enum class passed from constructor class

---
+++ lib/src/view/home/homemodel.dart
@@ -0,0 +1,22 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+enum SwitchButtonType { button1, button2, button3 }
+
+class HomeModel extends StateNotifier<Map<SwitchButtonType, bool>> {
+  HomeModel() : super({
+    SwitchButtonType.button1: false,
+    SwitchButtonType.button2: false,
+    SwitchButtonType.button3: false,
+
+  });
+  void toggleSwitch(SwitchButtonType buttonType) {
+    state = {
+      ...state,
+      buttonType: !(state[buttonType] ?? false),
+    };
+  }
+}
+
+final homeModelProvider = StateNotifierProvider<HomeModel, Map<SwitchButtonType, bool>>(
+      (ref) => HomeModel(),
+);
