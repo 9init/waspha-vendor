305e09b23ca2e7470441e4ef855294f1f9cbd157
MohamedMahmoud101198 <mohamed.mahmoud@appgain.io>
2023-12-03 22:16:03 +0200

    [fix]:fix checked if user logged in

--- lib/src/view/startup/startup.dart
+++ lib/src/view/startup/startup.dart
@@ -15,7 +15,7 @@ class StartUp extends StatelessWidget {
         Future(() {
           ref.watch(authCheckViewModelProvider).when(
                 data: (isLoggedIn) {
-                  isLoggedIn ? context.go("/login") : context.go("/main");
+                  isLoggedIn ? context.go("/main") : context.go("/login");
                 },
                 error: (error, stackStrace) {
                   context.go("/login");
