65d9dd36bd8401ef08c3df70a15ab43633ce3a37
MohamedMahmoud101198 <mohamed.mahmoud@appgain.io>
2024-01-05 01:37:35 +0200

    get_driver_bt=y_id

---
+++ lib/src/repository/carriers/get_driver_by_id/get_driver_by_id.dart
@@ -0,0 +1,14 @@
+import 'package:vendor/core/constans/api/end_points/waspha_vendor_end_points.dart';
+import 'package:vendor/src/models/get_driver_by_id/get_driver_by_id_request_model.dart';
+import 'package:vendor/src/models/get_driver_by_id/get_driver_by_id_response_model.dart';
+import 'package:vendor/src/shared/networking/Networking.dart';
+
+class GetDriverByIdRepository {
+  static Future<GetDriverByIdResponseModel> getDriverById({
+    required String driverId,
+  }) async {
+    final data = GetDriverByIdRequestModel(driverId: driverId).toJson();
+    final response =
+        await Networking.post(WasphaVendorEndPoints.getDriverById, data);
+  }
+}
