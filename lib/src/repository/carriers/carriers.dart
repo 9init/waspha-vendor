e683606cc754a353bc24b70ab1470a350126c366
9init <emovent2e@gmail.com>
2024-01-05 01:49:05 +0200

    move getDriverById to Carriers

--- lib/src/repository/carriers/carriers.dart
+++ lib/src/repository/carriers/carriers.dart
@@ -1,5 +1,8 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:vendor/core/constans/api/end_points/waspha_vendor_end_points.dart';
 import 'package:vendor/src/models/driver/driver_model.dart';
+import 'package:vendor/src/models/get_driver_by_id/get_driver_by_id_request_model.dart';
+import 'package:vendor/src/models/get_driver_by_id/get_driver_by_id_response_model.dart';
 import 'package:vendor/src/shared/networking/Networking.dart';
 import 'package:vendor/src/shared/networking/results.dart';
 
@@ -22,6 +25,14 @@ class CarriersRepository {
       _ => null,
     };
     return value;
+  }
+
+  static Future<GetDriverByIdResponseModel?> getDriverById({
+    required String driverId,
+  }) async {
+    final data = GetDriverByIdRequestModel(driverId: driverId).toJson();
+    final response =
+        await Networking.post(WasphaVendorEndPoints.getDriverById, data);
   }
 
   static final storeDriverProvider =
