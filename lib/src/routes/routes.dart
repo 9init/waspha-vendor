3eedc822082474edf68ddb2fefd122a9b5a14a7b
MohamedMahmoud101198 <mohamed.mahmoud@appgain.io>
2024-01-08 16:38:55 +0200

    [feat]:added new routes and routes names for making it dynamic

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,5 +1,6 @@
 import 'package:go_router/go_router.dart';
 import 'package:vendor/src/models/driver/driver_model.dart';
+import 'package:vendor/src/routes/routes_names.dart';
 import 'package:vendor/src/view/carrier/carrier.dart';
 import 'package:vendor/src/view/carriers_management/carriers_management.dart';
 import 'package:vendor/src/view/forget_password/forget_password.dart';
@@ -18,6 +19,9 @@ import 'package:vendor/src/view/payment_methods/payment_methods.dart';
 import 'package:vendor/src/view/commissions/commissions.dart';
 import 'package:vendor/src/view/store_profile/store_profile.dart';
 import 'package:vendor/src/view/store_reviews/store_reviews.dart';
+import 'package:vendor/src/view/update_driver_data/screens/add_new_driver_screen/add_new_driver_screen.dart';
+import 'package:vendor/src/view/update_driver_data/screens/contact_list_screen/contact_list_screen.dart';
+import 'package:vendor/src/view/update_driver_data/screens/update_driver_data_screen/update_driver_data_screen.dart';
 import 'package:vendor/src/view/verification/verification.dart';
 
 import '../view/application_form/application_form.dart';
@@ -25,81 +29,103 @@ import '../view/notification/notification.dart';
 import '../view/payouts/payouts.dart';
 import '../view/verification/verification_waiting.dart';
 
-final router = GoRouter(initialLocation: '/', routes: [
-  GoRoute(path: '/', builder: (context, state) => const StartUp()),
-  GoRoute(path: '/main', builder: (context, state) => const Navigation()),
-  GoRoute(path: '/login', builder: (context, state) => Login()),
-  GoRoute(path: '/register', builder: (context, state) => Register()),
-  GoRoute(path: '/forget_pass', builder: (context, state) => ForgetPassword()),
-  GoRoute(
-    path: '/forget_pass_otp',
-    builder: (context, state) => ForgetPasswordOtp(
-      resetModel: state.extra as PasswordResetModel,
+final router = GoRouter(
+  debugLogDiagnostics: true,
+  initialLocation: '/',
+  routes: [
+    GoRoute(path: '/', builder: (context, state) => const StartUp()),
+    GoRoute(path: '/main', builder: (context, state) => const Navigation()),
+    GoRoute(path: '/login', builder: (context, state) => Login()),
+    GoRoute(path: '/register', builder: (context, state) => Register()),
+    GoRoute(
+        path: '/forget_pass', builder: (context, state) => ForgetPassword()),
+    GoRoute(
+      path: '/forget_pass_otp',
+      builder: (context, state) => ForgetPasswordOtp(
+        resetModel: state.extra as PasswordResetModel,
+      ),
     ),
-  ),
-  GoRoute(
-    path: '/reset_pass',
-    builder: (context, state) =>
-        ResetPassword(followUpCode: state.extra as String),
-  ),
-  GoRoute(
-    path: '/verification',
-    builder: (context, state) => Verification(),
-  ),
-  GoRoute(
-    path: '/payouts',
-    builder: (context, state) => const Payouts(),
-  ),
-  GoRoute(
-    path: '/settlement',
-    builder: (context, state) => const Settlement(),
-  ),
-  GoRoute(
-    path: '/receipt',
-    builder: (context, state) => const Receipt(),
-  ),
-  GoRoute(
-    path: '/order',
-    builder: (context, state) => const Order(),
-  ),
-  GoRoute(
-    path: '/requests',
-    builder: (context, state) => const Requests(),
-  ),
-  GoRoute(
-    path: '/notification',
-    builder: (context, state) => const Notifications(),
-  ),
-  GoRoute(
-    path: '/application_form',
-    builder: (context, state) => const ApplicationForm(),
-  ),
-  GoRoute(
-    path: '/verification_waiting',
-    builder: (context, state) => VerificationWaiting(),
-  ),
-  GoRoute(
-    path: '/store_profile',
-    builder: (context, state) => StoreProfile(),
-  ),
-  GoRoute(
-    path: '/commissions',
-    builder: (context, state) => Commissions(),
-  ),
-  GoRoute(
-    path: "/payment_methods",
-    builder: (context, state) => PaymentMethods(),
-  ),
-  GoRoute(
-    path: "/store_reviews",
-    builder: (context, state) => StoreReviewsScreen(),
-  ),
-  GoRoute(
-    path: "/carriers_management",
-    builder: (context, state) => CarrierManagement(),
-  ),
-  GoRoute(
-    path: "/carrier",
-    builder: (context, state) => Carrier(driverType: state.extra as DriverType),
-  )
-]);
+    GoRoute(
+      path: '/reset_pass',
+      builder: (context, state) =>
+          ResetPassword(followUpCode: state.extra as String),
+    ),
+    GoRoute(
+      path: '/verification',
+      builder: (context, state) => Verification(),
+    ),
+    GoRoute(
+      path: '/payouts',
+      builder: (context, state) => const Payouts(),
+    ),
+    GoRoute(
+      path: '/settlement',
+      builder: (context, state) => const Settlement(),
+    ),
+    GoRoute(
+      path: '/receipt',
+      builder: (context, state) => const Receipt(),
+    ),
+    GoRoute(
+      path: '/order',
+      builder: (context, state) => const Order(),
+    ),
+    GoRoute(
+      path: '/requests',
+      builder: (context, state) => const Requests(),
+    ),
+    GoRoute(
+      path: '/notification',
+      builder: (context, state) => const Notifications(),
+    ),
+    GoRoute(
+      path: '/application_form',
+      builder: (context, state) => const ApplicationForm(),
+    ),
+    GoRoute(
+      path: '/verification_waiting',
+      builder: (context, state) => VerificationWaiting(),
+    ),
+    GoRoute(
+      path: '/store_profile',
+      builder: (context, state) => StoreProfile(),
+    ),
+    GoRoute(
+      path: '/commissions',
+      builder: (context, state) => Commissions(),
+    ),
+    GoRoute(
+      path: "/payment_methods",
+      builder: (context, state) => PaymentMethods(),
+    ),
+    GoRoute(
+      path: "/store_reviews",
+      builder: (context, state) => StoreReviewsScreen(),
+    ),
+    GoRoute(
+      path: "/carriers_management",
+      builder: (context, state) => CarrierManagement(),
+    ),
+    GoRoute(
+      path: "/carrier",
+      builder: (context, state) =>
+          Carrier(driverType: state.extra as DriverType),
+    ),
+    GoRoute(
+        name: RoutesNames.updateDriverDataScreen,
+        path: '${RoutesNames.updateDriverDataScreen}/:driver_id',
+        builder: (context, state) {
+          return UpdateDriverDataScreen(
+            driverId: state.pathParameters['driver_id']!,
+          );
+        }),
+    GoRoute(
+      path: RoutesNames.contactListScreen,
+      builder: (context, state) => ContactListScreen(),
+    ),
+    GoRoute(
+      path: RoutesNames.addNewDriver,
+      builder: (context, state) => AddNewDriverScreen(),
+    ),
+  ],
+);
---
+++ lib/src/routes/routes_names.dart
@@ -0,0 +1,5 @@
+class RoutesNames{
+  static const String updateDriverDataScreen='/updateDriverDataScreen';
+  static const String contactListScreen='/contactListScreen';
+  static const String addNewDriver='/addNewDriver';
+}
