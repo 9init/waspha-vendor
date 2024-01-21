3f9388fed7922c324089fa65efc7f1a01e62a090
MohamedMahmoud101198 <mohamed.mahmoud@appgain.io>
2024-01-21 04:19:53 +0200

    [feat]=:make some edits on home responsive

--- lib/src/view/common/profile_app_bar/profile_app_bar.dart
+++ lib/src/view/common/profile_app_bar/profile_app_bar.dart
@@ -1,36 +1,43 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:vendor/core/gen/assets.gen.dart';
 import 'package:vendor/src/repository/auth/login.dart';
+import 'package:vendor/src/routes/routes_names.dart';
 import 'package:vendor/src/view/home/viewmodel.dart';
 
 class ProfileAppBar extends ConsumerWidget implements PreferredSizeWidget {
   ProfileAppBar({
     super.key,
   });
+
   final AppBar appBar = AppBar();
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final homeModel = ref.watch(homeModelProvider);
     final vendor = ref.watch(LoginRepository.vendorProvider);
+    debugPrint('The Vendor profile Is ${vendor.value?.avatar}');
     return AppBar(
       leading: Padding(
         padding: const EdgeInsets.only(left: 14),
-        child: CircleAvatar(
-          backgroundImage: !vendor.hasValue
-              ? AssetImage($AssetsImgsGen().avatar.path) as ImageProvider
-              : CachedNetworkImageProvider(vendor.value?.avatar??''),
+        child: GestureDetector(
+          onTap: () => context.push(
+            RoutesNames.profileScreen,
+          ),
+          child: CircleAvatar(
+            backgroundImage: !vendor.hasValue
+                ? AssetImage($AssetsImgsGen().avatar.path) as ImageProvider
+                : CachedNetworkImageProvider(vendor.value?.avatar ?? ''),
+          ),
         ),
       ),
       title: Align(
         alignment: Alignment.topLeft,
         child: Text(
           "Home",
-          style: TextStyle(fontSize: 70.sp, fontWeight: FontWeight.bold),
+          style: Theme.of(context).textTheme.titleLarge,
         ),
       ),
       actions: [
@@ -46,7 +53,7 @@ class ProfileAppBar extends ConsumerWidget implements PreferredSizeWidget {
             },
             child: CircleAvatar(
               backgroundImage: homeModel.storeImage == null
-                  ? AssetImage($AssetsImgsGen().storeLogo.path) as ImageProvider
+                  ? AssetImage(MyAssets.imgs.storeLogo.path) as ImageProvider
                   : CachedNetworkImageProvider(homeModel.storeImage!),
             ),
           ),
