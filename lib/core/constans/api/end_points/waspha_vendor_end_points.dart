e153778b4352639367ebe2bb88d90c98e1d0df54
MohamedMahmoud101198 <mohamed.mahmoud@appgain.io>
2024-01-21 04:13:23 +0200

    [feat]:added some endpoints

--- lib/core/constans/api/end_points/waspha_vendor_end_points.dart
+++ lib/core/constans/api/end_points/waspha_vendor_end_points.dart
@@ -3,4 +3,6 @@ class WasphaVendorEndPoints{
   static const String appSettings='/app-settings';
   static const String deleteDriver='/delete-driver';
   static const String editDriver='/edit-driver';
+  static const String addDriver='/add-driver';
+  static const String transactionsListing='/transactions-listing';
 }
