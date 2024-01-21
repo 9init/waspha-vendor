30350547688243181710467d119bdea93cf55d89
MohamedMahmoud101198 <mohamed.mahmoud@appgain.io>
2024-01-21 04:20:29 +0200

    [feat]:added some static routes name and implement new screens for routes

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -3,6 +3,7 @@ import 'package:vendor/src/models/driver/driver_model.dart';
 import 'package:vendor/src/routes/routes_names.dart';
 import 'package:vendor/src/view/carrier/carrier.dart';
 import 'package:vendor/src/view/carriers_management/carriers_management.dart';
+import 'package:vendor/src/view/driver/driver_details_screen/driver_details_screen.dart';
 import 'package:vendor/src/view/forget_password/forget_password.dart';
 import 'package:vendor/src/view/forget_password/viewmodel.dart';
 import 'package:vendor/src/view/forget_password_otp/forget_password_otp.dart';
@@ -19,21 +20,27 @@ import 'package:vendor/src/view/payment_methods/payment_methods.dart';
 import 'package:vendor/src/view/commissions/commissions.dart';
 import 'package:vendor/src/view/store_profile/store_profile.dart';
 import 'package:vendor/src/view/store_reviews/store_reviews.dart';
+import 'package:vendor/src/view/transactions-listing/screens/transactions-listing_screen/transactions-listing_screen.dart';
 import 'package:vendor/src/view/update_driver_data/screens/add_new_driver_screen/add_new_driver_screen.dart';
 import 'package:vendor/src/view/update_driver_data/screens/contact_list_screen/contact_list_screen.dart';
 import 'package:vendor/src/view/update_driver_data/screens/update_driver_data_screen/update_driver_data_screen.dart';
+import 'package:vendor/src/view/vendor_profile/screens/vendor_profile_deltails_screen/vendor_profile_details_screen.dart';
+import 'package:vendor/src/view/vendor_profile/screens/vendor_profile_screen/vendor_profile_screen.dart';
 import 'package:vendor/src/view/verification/verification.dart';
+import 'package:vendor/src/view/verification/verification_waiting.dart';
 
 import '../view/application_form/application_form.dart';
 import '../view/notification/notification.dart';
 import '../view/payouts/payouts.dart';
-import '../view/verification/verification_waiting.dart';
 
 final router = GoRouter(
   debugLogDiagnostics: true,
   initialLocation: '/',
   routes: [
-    GoRoute(path: '/', builder: (context, state) => const StartUp()),
+    GoRoute(
+      path: '/',
+      builder: (context, state) => StartUp(),
+    ),
     GoRoute(path: '/main', builder: (context, state) => const Navigation()),
     GoRoute(path: '/login', builder: (context, state) => Login()),
     GoRoute(path: '/register', builder: (context, state) => Register()),
@@ -121,11 +128,33 @@ final router = GoRouter(
         }),
     GoRoute(
       path: RoutesNames.contactListScreen,
-      builder: (context, state) => ContactListScreen(),
+      builder: (context, state) => const ContactListScreen(),
     ),
     GoRoute(
       path: RoutesNames.addNewDriver,
-      builder: (context, state) => AddNewDriverScreen(),
+      builder: (context, state) => AddNewDriverScreen(
+        driverType: state.extra as DriverType,
+      ),
+    ),
+    GoRoute(
+      path: RoutesNames.profileScreen,
+      builder: (context, state) => VendorProfileScreen(),
+    ),
+    GoRoute(
+      path: RoutesNames.profileDetailScreen,
+      builder: (context, state) => VendorProfileDetailsScreen(),
+    ),
+    GoRoute(
+      path: '${RoutesNames.driverDetailsScreen}/:driver_id',
+      name: RoutesNames.driverDetailsScreen,
+      builder: (context, state) => DriverDetailsScreen(
+        driverId: state.pathParameters['driver_id']!,
+      ),
+    ),
+    GoRoute(
+      path: RoutesNames.transactionsListingScreen,
+      builder: (context, state) => TransactionsListingScreen(
+      ),
     ),
   ],
 );
--- lib/src/routes/routes_names.dart
+++ lib/src/routes/routes_names.dart
@@ -2,4 +2,8 @@ class RoutesNames{
   static const String updateDriverDataScreen='/updateDriverDataScreen';
   static const String contactListScreen='/contactListScreen';
   static const String addNewDriver='/addNewDriver';
+  static const String profileScreen='/profileScreen';
+  static const String profileDetailScreen='/profileDetailScreen';
+  static const String driverDetailsScreen='/driverDetailsScreen';
+  static const String transactionsListingScreen='/transactionsListingScreen';
 }
