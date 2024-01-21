3df13513832ef744bd9210689894c750f43b0dc8
MohamedMahmoud101198 <mohamed.mahmoud@appgain.io>
2024-01-21 04:21:51 +0200

    [feat]:edit on theme on light

--- lib/Themes/light_theme.dart
+++ lib/Themes/light_theme.dart
@@ -6,15 +6,13 @@ ThemeData lightThemeData() {
   return ThemeData(
     useMaterial3: true,
     brightness: Brightness.light,
-
-    //Setting the Brightness to light  so that this can be used as Light ThemeData
-    textTheme: CustomTextTheme.textTheme(),
-    //Setting the Text Theme to LightTextTheme
-    appBarTheme: const AppBarTheme(
-      iconTheme: IconThemeData(
-        color: WasphaColors.whiteColor,
+    actionIconTheme: ActionIconThemeData(
+      backButtonIconBuilder: (BuildContext context) => Icon(
+        Icons.arrow_back_ios,
       ),
     ),
+    //Setting the Brightness to light  so that this can be used as Light ThemeData
+    textTheme: CustomTextTheme.textTheme(),
     dialogBackgroundColor: WasphaColors.whiteColor,
   );
 }
